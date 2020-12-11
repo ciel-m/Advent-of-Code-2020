d552b8990a64d37ca9d18041d0f92ea49cfb5a7c
CIEL <ciel-m@users.noreply.github.com>
2020-12-11 12:45:11 -0500

    d3p2 sol

--- day3.py
+++ day3.py
@@ -1,14 +1,14 @@
 f = open('input3.txt', 'r')
 mapping = [x.strip() for x in f]
-row, col = 0, 0
-trees = 0
-while row < len(mapping):
-    if mapping[row][col] == "#":
-        trees += 1
-    #slope of 3, 1
-    row += 1
-    col += 3
-    if col >= len(mapping[0]):
-        col %= len(mapping[0])
-    print(col)
-print("trees: ", trees)
+def slide(run, rise):
+    row, col = 0, 0
+    trees = 0
+    while row < len(mapping):
+        if mapping[row][col] == "#":
+            trees += 1
+        row += rise
+        col += run
+        if col >= len(mapping[0]):
+            col %= len(mapping[0])
+    return trees
+print(slide(1,1)*slide(3,1)*slide(5,1)*slide(7,1)*slide(1,2))
